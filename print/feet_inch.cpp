d65ef0f4a18a629ee8dd0c622a78eccc5dd6521f
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2024-01-25 22:38:52 +0530

    Update feet_inch.cpp

--- print/feet_inch.cpp
+++ print/feet_inch.cpp
@@ -1,1 +1,16 @@
-
+ #include<iostream>
+ using namespace std;
+ main()
+ {
+ 	int inch,feet;
+ 	cout << "Enter feet : ";
+ 	cin >> feet;
+ 	cout << "Enter inches : ";
+ 	cin >> inch;
+ 	int i;
+ 	for(i=0;inch>=12;i++)
+ 	{
+ 	    inch-=12;	
+	}
+	cout << "\nFeet - " << feet+i <<" , "<< "Inch - " << inch;
+ }
