7d0ba9518f81ac158d8614d06022d2954528a8c8
sadhana chaudhary <148869257+sadhana5953@users.noreply.github.com>
2024-02-24 19:28:03 +0530

    Update interest.cpp

--- bank/interest.cpp
+++ bank/interest.cpp
@@ -1,71 +1,50 @@
-//Q.1 Write a Program to print the rate of interest of different banks using hierarchical inheritance:
-//- Class RBI -> Class SBI
-//- Class RBI -> Class BOB
-//- Class RBI -> Class ICICI
-//- inherit rate attribute and getROI() method in all child
-//classes
-
 #include<iostream>
 using namespace std;
 class RBI
 {
-	public :
-	float Interest;
+	protected:
+	int Interest;
 	
-	void getROI()
+	void set()
 	{
-		cout << "Rate of interest of RBI: " << Interest << "%" << endl;
+		cout << "Enter the rate of interest :";
+		cin >> Interest;
 	}
 };
-class SBI : public RBI
+class SBI :public RBI
 {
 	public :
-	float Interest;
 	
 	void getROI()
 	{
-		cout << "Rate of interest of SBI: " << SBI::Interest << "%" << endl;
+		set();
+		cout << "Rate of interest of SBI: " << Interest << "%" << endl;
 	}
 };
-class BOB : public RBI
+class BOB :public RBI
 {
-	public :
-	float Interest;
-	
+	public:
 	void getROI()
 	{
-		cout << "Rate of interest of BOB: " << BOB::Interest << "%" << endl;
+		set();
+		cout << "Rate of interest of BOB: " << Interest << "%" << endl;
 	}
 };
-class ICICI : public RBI
+class ICICI :public RBI
 {
-	public :
-	float Interest;
-	
+	public:
 	void getROI()
 	{
-		cout << "Rate of interest of ICICI: " << ICICI::Interest << "%" << endl;
+		set();
+		cout << "Rate of interest of ICICI: " << Interest << "%" << endl;
 	}
 };
 main()
 {
-	RBI rate1;
-	SBI rate2;
-    BOB rate3;
-    ICICI rate4;
-    
-	cout << "Enter rate of interest for RBI: ";
-    cin >> rate1.Interest;
-    cout << "Enter rate of interest for SBI: ";
-    cin >> rate2.Interest;
-    cout << "Enter rate of interest for BOB: ";
-    cin >> rate3.Interest;
-    cout << "Enter rate of interest for ICICI: ";
-    cin >> rate4.Interest;
-    
-    rate1.getROI();
-    rate2.getROI();
-    rate3.getROI();
-    rate4.getROI();
-    
+	SBI s1;
+	BOB b1;
+    ICICI c1;
+	s1.getROI();
+	b1.getROI();
+	c1.getROI();
 }
